d5ba0f2127ef373826839542cce4bf0e00d5eb19
Matthew Joseph <matthew.j.joe@gmail.com>
2021-07-02 10:51:33 -0400

    Added in components for header
    Just added the actual bar that holds the Spotify logo at the moment. Will be adding the 'Profile Menu' next.

--- SpotifyProfile/src/app/app.component.ts
+++ SpotifyProfile/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  encapsulation: ViewEncapsulation.None //had to add this to get SASS file let me style the body tag
 })
 export class AppComponent {
   title = 'SpotifyProfile';
---
+++ SpotifyProfile/src/app/components/header-profile-menu/header-profile-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-profile-menu',
+  templateUrl: './header-profile-menu.component.html',
+  styleUrls: ['./header-profile-menu.component.sass']
+})
+export class HeaderProfileMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
