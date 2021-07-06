b4431cbfcb8faac8648ce0137d5801bcad27c1a7
Matthew Joseph <matthew.j.joe@gmail.com>
2021-07-06 10:21:42 -0400

    Added HTTP module and server.js file

--- SpotifyProfile/src/app/app.module.ts
+++ SpotifyProfile/src/app/app.module.ts
@@ -5,8 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeaderProfileMenuComponent } from './components/header-profile-menu/header-profile-menu.component';
+import { HttpClientModule } from '@angular/common/http';
 
-@NgModule({
+
+@NgModule({ 
   declarations: [
     AppComponent,
     HeaderComponent,
@@ -14,7 +16,8 @@ import { HeaderProfileMenuComponent } from './components/header-profile-menu/hea
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
