174f976cac821748bd40170b0b17f946e6c28b65
Matthew Joseph <matthew.j.joe@gmail.com>
2021-08-04 08:01:53 -0400

    Added routing in angular and removed unused dependences

--- SpotifyProfile/src/server/server.js
+++ SpotifyProfile/src/server/server.js
@@ -1,24 +1,5 @@
-const express = require('express');
-const SpotifyWebApi = require('spotify-web-api');
+import axios from 'axios';
 
-const app = express();
 
-app.post('/login', (req, res) => {
-    const code = req.body.code;
-    const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:4200/',
-        clientId: '53e2679e63cc4cae9f6b24b8013fd15f',
-        clientSecret:'a8a35d1d5f12460e87c0cf29d3c89851' //remove this before pushing to prod.
-    })
+//When you receive the authorization code, you will need to exchange it with an access token
 
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-        res.json({ 
-           accessToken: data.body.access_token,
-            refresh_token: data.body.refresh_token,
-            expires_in: data.body.expires_in
-        }) 
-    })
-    .catch(() => {
-        res.sendStatus(400)
-    })
-})
