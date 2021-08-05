71e9e10f92e4c26f71f59f507f9f3247c789da4e
Matthew Joseph <matthew.j.joe@gmail.com>
2021-08-04 23:51:39 -0400

    Added POST req to /api/token endpoint to get access_token
    Program now gets access_token

--- SpotifyProfile/src/app/components/dashboard/dashboard.component.ts
+++ SpotifyProfile/src/app/components/dashboard/dashboard.component.ts
@@ -12,20 +12,24 @@ import axios from 'axios';
 export class DashboardComponent implements OnInit {
 
   constructor() { 
-    //code from api request
+    //code from initial api request
     const code = new URLSearchParams(window.location.search).get('code');
-    console.log("Your access code is: " + code);
 
-    //check if code exist and do post req based on that
     if(code){
-      axios.post('https://accounts.spotify.com/api/token', {
-        headers: {
-          Authorization: 'Basic NTNlMjY3OWU2M2NjNGNhZTlmNmIyNGI4MDEzZmQxNWY6YjZhODljNjM0NGI5NGU3NGIwMGJiMzMzN2U3NmM4NTI'
-        },
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: 'http://localhost:4200/dashboard'
-      })
+      //(spotify requries body to be sent application/x-www-form-urlencoded * axios sends data by default as JSON)
+      const encodedParams = new URLSearchParams();
+      encodedParams.append('grant_type', 'authorization_code');
+      encodedParams.append('code', code);
+      encodedParams.append('redirect_uri', 'http://localhost:4200/dashboard');
+
+      axios.post('https://accounts.spotify.com/api/token', encodedParams ,
+        {
+          headers: {
+            //Auth format = Authorization: Basic *<base64 encoded client_id:client_secret>*
+            Authorization: 'Basic NTNlMjY3OWU2M2NjNGNhZTlmNmIyNGI4MDEzZmQxNWY6YjZhODljNjM0NGI5NGU3NGIwMGJiMzMzN2U3NmM4NTI'
+          }
+        }
+      )
       .then(function (response: any) {
         console.log(response);
       })
